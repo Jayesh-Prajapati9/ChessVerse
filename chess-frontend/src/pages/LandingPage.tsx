6b4576f48e524c3740547ae6e5c1ea18abab541d
Jayesh Prajapati <jayeshprajapati.0904@gmail.com>
2025-07-14 23:20:42 +0530

    Bg animation

--- chess-frontend/src/pages/LandingPage.tsx
+++ chess-frontend/src/pages/LandingPage.tsx
@@ -37,7 +37,7 @@ const LandingPage = () => {
 		>
 			{/* Subtle Background Pattern */}
 			<div className="fixed inset-0 overflow-hidden pointer-events-none">
-				{/* <div
+				<div
 					className={`absolute -top-40 -right-40 w-80 h-80 rounded-full blur-3xl opacity-5 animate-pulse ${
 						isDark ? "bg-gray-400" : "bg-gray-600"
 					}`}
@@ -47,13 +47,13 @@ const LandingPage = () => {
 					className={`absolute -bottom-40 -left-40 w-96 h-96 rounded-full blur-3xl opacity-5 animate-pulse delay-1000 ${
 						isDark ? "bg-gray-500" : "bg-gray-500"
 					}`}
-				></div> */}
+				></div>
 
-				{/* <div
+				<div
 					className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-64 h-64 rounded-full blur-3xl opacity-3 animate-pulse delay-500 ${
 						isDark ? "bg-gray-600" : "bg-gray-400"
 					}`}
-				></div> */}
+				></div>
 			</div>
 
 			{/* Navigation */}
