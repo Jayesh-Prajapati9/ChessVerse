19d94563c7fb15522ca57057ddca417acff6b8fe
Jayesh-Prajapati9 <jayeshprajapati.0904@gmail.com>
2025-08-08 22:18:41 +0530

    feat: set up Turborepo project with base configuration

--- chess-backend/db/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '../generated/prisma';
-import { PrismaClientKnownRequestError } from "../generated/prisma/runtime/library";
-export declare const prismaClient: PrismaClient<import("../generated/prisma").Prisma.PrismaClientOptions, never, import("../generated/prisma/runtime/library").DefaultArgs>;
-export declare const prismaError: typeof PrismaClientKnownRequestError;
-export * from "./dbQueries/userQueries";
-//# sourceMappingURL=index.d.ts.map
