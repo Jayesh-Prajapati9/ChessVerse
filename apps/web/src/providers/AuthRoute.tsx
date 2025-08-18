dda8830f604a9bce5296c690806e8f78add9d8ad
Jayesh-Prajapati9 <jayeshprajapati.0904@gmail.com>
2025-08-18 21:38:41 +0530

    fix(auth): rely on loader for user context to avoid duplicate API calls

--- apps/web/src/providers/AuthRoute.tsx
+++ apps/web/src/providers/AuthRoute.tsx
@@ -2,12 +2,18 @@ import { useEffect } from "react";
 import { Outlet, useLoaderData } from "react-router-dom";
 import type { UserWithStats } from "../hooks/useUser";
 import { useUser } from "../hooks/useUser";
+
 export const AuthRoute = () => {
 	const { setUser } = useUser();
-	const userData = useLoaderData() as UserWithStats;
-    useEffect(() => {
-        console.log(userData);
-		setUser((prev) => userData ?? prev);
+	const userData = useLoaderData() as UserWithStats | null;
+
+	useEffect(() => {
+		if (userData) {
+			setUser(userData);
+		} else {
+			setUser(null);
+		}
 	}, [userData, setUser]);
-	return <Outlet/>;
+
+	return <Outlet />;
 };
